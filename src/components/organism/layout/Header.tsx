a01ed2c1e3b0225472ec25f74f4e524d8b277e4c
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-26 07:46:20 +0900

    2222

--- src/components/organism/layout/Header.tsx
+++ src/components/organism/layout/Header.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Heading, Link } from "@chakra-ui/react";
+import { Box, Flex, Heading, IconButton, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
   return (
@@ -11,9 +12,11 @@ export const Header: VFC = memo(() => {
       justify="space-between"
       padding={{ base: 3, md: 5 }}
     >
-      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-        ユーザー管理アプリ
-      </Heading>
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          ユーザー管理アプリ
+        </Heading>
+      </Flex>
       <Flex
         align="center"
         fontSize="sm"
@@ -25,6 +28,13 @@ export const Header: VFC = memo(() => {
         </Box>
         <Link>設定</Link>
       </Flex>
+      <IconButton
+        aria-label="メニューボタン"
+        icon={<HamburgerIcon />}
+        size="sm"
+        variant="unstyled"
+        display={{ base: "block", md: "none" }}
+      />
     </Flex>
   );
 });
