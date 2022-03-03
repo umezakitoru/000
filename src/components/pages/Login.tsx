6ae6c5dab29afc06e0313ae1ae6470c82c92591f
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-03-03 18:46:15 +0900

    111

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,8 +1,11 @@
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useState, VFC } from "react";
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState("");
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,7 +14,11 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
+          <Input
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
           <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
