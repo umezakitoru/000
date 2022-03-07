a0f5878a34db88cbbb9939c522cdcbb06b5aab69
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-03-08 07:03:36 +0900

    111

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,16 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理ページです。</p>;
+  return (
+    <Wrap>
+      {[...Array(10)].map(() => (
+        <WrapItem>
+          <div
+            style={{ width: "100px", height: "100px", backgroundColor: "teal" }}
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 });
