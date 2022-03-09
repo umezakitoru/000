85e62a78794d56b7bbb6df42ac4622683f5e60a8
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-03-10 07:19:27 +0900

    111

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,35 +1,17 @@
 import { memo, VFC } from "react";
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organism/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="プロフィール画像"
-              margin="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              うめ
-            </Text>
-            <Text fontSize="sm" color="gray">
-              umezaki toru
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="うめ"
+          fullName="umezaki toru"
+        />
       </WrapItem>
     </Wrap>
   );
