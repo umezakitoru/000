f6766ce6215386d588f06c4788c4bce150d8b336
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-03-10 07:05:53 +0900

    111

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -3,7 +3,7 @@ import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap>
+    <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
         <Box
           w="260px"
