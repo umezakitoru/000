c3e853dc9d41496416379e33205a2b8119758bca
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-03-12 07:40:00 +0900

    111

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,32 @@
-import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { UserCard } from "../organism/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="うめ"
-          fullName="umezaki toru"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {true ? (
+        <Center h="100hv">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          <WrapItem>
+            <UserCard
+              imageUrl="https://source.unsplash.com/random"
+              userName="うめ"
+              fullName="umezaki toru"
+            />
+          </WrapItem>
+        </Wrap>
+      )}
+    </>
   );
 });
--- src/hooks/useAllUsers.ts
+++ src/hooks/useAllUsers.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useState } from "react";
 import axios from "axios";
 
