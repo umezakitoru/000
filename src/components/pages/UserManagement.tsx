fdeb193a12325ccaa2edf5567b90cc1eb4685f2b
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-03-17 07:14:59 +0900

    111

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -11,15 +11,19 @@ import {
 import { UserCard } from "../organism/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organism/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
+  console.log(selectedUser);
 
   useEffect(() => getUsers(), []);
 
   const onClickUser = useCallback((id: number) => {
     console.log(id);
+    onSelectUser({ id, users });
     onOpen();
   }, []);
 
--- src/hooks/useSelectUser.ts
+++ src/hooks/useSelectUser.ts
@@ -1,7 +1,17 @@
 import { useCallback, useState } from "react";
+import { User } from "../types/api/user";
+
+type Props = {
+  id: number;
+  users: Array<User>;
+};
 
 export const useSelectuser = () => {
-  const [selectUser, setSelectUser] = useState();
-  const onSelectUser = useCallback(() => {}, []);
-  return { onSelectUser, selectUser };
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const onSelectUser = useCallback((props: Props) => {
+    const { id, users } = props;
+    const targetUser = users.find((user) => user.id === id);
+    setSelectedUser(targetUser ?? null);
+  }, []);
+  return { onSelectUser, selectedUser };
 };
